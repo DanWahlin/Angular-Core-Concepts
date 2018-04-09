6ffa320408a7d3f88a4018af861191c566203a69
Dan Wahlin <danwahlin@Dans-iMac.local>
2018-04-08 18:21:19 -0700

    Moved calculateOrders to correct position

--- src/app/customers/customers-list/customers-list.component.ts
+++ src/app/customers/customers-list/customers-list.component.ts
@@ -42,10 +42,10 @@ export class CustomersListComponent implements OnInit {
                        cust.city.toLowerCase().indexOf(data.toLowerCase()) > -1 ||
                        cust.orderTotal.toString().indexOf(data) > -1;
             });
-            this.calculateOrders();
         } else {
             this.filteredCustomers = this.customers;
         }
+        this.calculateOrders();
     }
 
     sort(prop: string) {
