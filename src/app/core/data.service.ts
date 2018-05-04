90ea06fb6f9afebc96967140208c0423f7288769
Dan Wahlin <danwahlin@Dans-iMac.local>
2018-05-04 13:36:13 -0700

    Update to Angular 6

--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
 
 import { ICustomer, IOrder } from '../../app/shared/interfaces';
