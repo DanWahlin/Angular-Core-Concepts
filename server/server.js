1f66170e139feb928ac545010aac54a02d589864
Dan Wahlin <dwahlin@xmlforasp.net>
2021-05-26 20:22:41 -0700

    Continue update to 12

--- server/server.js
+++ server/server.js
@@ -1,15 +1,14 @@
 "use strict";
 var express     = require('express'),
-    bodyParser  = require('body-parser'),
     app         = express(),
     customers   = require('./data/customers'),
     orders      = require('./data/orders');
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //CORS
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, X-XSRF-TOKEN, Content-Type, Accept");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
